f7d2a8de278b549a6b747e045c8accdc264aa426
dqnilka <makarov01V@ya.ru>
2024-05-02 12:29:45 +0300

    table-changes

--- train_bert/train_bert.py
+++ train_bert/train_bert.py
@@ -20,12 +20,15 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class_weights = (1 - (df['labels'].value_counts().sort_index() / len(df))).values
 class_weights = torch.from_numpy(class_weights).float().to(device)
 
+
 def get_dataset(csv_path, test_size=0.2):
     full_dataset = load_dataset('csv', data_files=csv_path)
     full_dataset = full_dataset.filter(lambda example: example['text'] is not None and example['labels'] is not None)
     full_dataset = full_dataset.class_encode_column("labels")
     dataset = full_dataset['train'].train_test_split(test_size=test_size, stratify_by_column='labels')
     return dataset
+
+
 dataset = get_dataset('data/data.csv')
 labels = sorted(df['labels'].value_counts().keys())
 
@@ -39,16 +42,19 @@ model_name = "DeepPavlov/rubert-base-cased"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)
 
+
 def preprocess_function(examples):
     return tokenizer(examples["text"], truncation=True)
+
 
 tokenized_dataset = dataset.map(preprocess_function, batched=True)
 
 from transformers import DataCollatorWithPadding
+
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(labels), id2label=id2label, label2id=label2id)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(labels), id2label=id2label,
+                                                           label2id=label2id)
 
 model.to(device)
 
@@ -60,11 +66,10 @@ optimizer = AdamW(model.parameters(), lr=2e-5)
 num_epochs = 3
 num_training_steps = num_epochs * dataset_len
 
-
 lr_scheduler = get_scheduler(
     "cosine",
     optimizer=optimizer,
-    num_warmup_steps= int(0.1 * num_epochs * dataset_len),
+    num_warmup_steps=int(0.1 * num_epochs * dataset_len),
     num_training_steps=num_training_steps
 )
 
@@ -72,13 +77,13 @@ lr_scheduler = get_scheduler(
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.get("labels")
-        
+
         outputs = model(**inputs)
         logits = outputs.get('logits')
-        
+
         loss_fct = nn.CrossEntropyLoss(weight=class_weights)
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
-        
+
         return (loss, outputs) if return_outputs else loss
 
 
@@ -102,13 +107,15 @@ training_args = TrainingArguments(
     greater_is_better=True,
     eval_accumulation_steps=32,
     fp16=True,  # mixed precision
-    load_best_model_at_end = True,
+    load_best_model_at_end=True,
 )
+
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return f1_metric.compute(predictions=predictions, references=labels, average='macro')
+
 
 trainer = CustomTrainer(
     model=model,
@@ -123,19 +130,19 @@ trainer = CustomTrainer(
 
 trainer.train()
 
-
 from tqdm import tqdm
-
 
 y_pred = []
 y_true = tokenized_dataset["test"]['labels']
 with torch.no_grad():
     for i in tqdm(range(len(tokenized_dataset["test"]))):
-        logits = model(**tokenizer(tokenized_dataset["test"][i]['text'], max_length=512, truncation=True, return_tensors="pt").to(device))
-    
+        logits = model(
+            **tokenizer(tokenized_dataset["test"][i]['text'], max_length=512, truncation=True, return_tensors="pt").to(
+                device))
+
         predicted_class_id = logits.logits.argmax().item()
         # model.config.id2label[predicted_class_id]
-    
+
         y_pred.append(predicted_class_id)
 
 print(f1_score(y_true, y_pred, average='macro'))
